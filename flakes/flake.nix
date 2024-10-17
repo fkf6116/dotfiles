6cb36548f90105aa0ee460971b315f4b76b4c15f
fruity-fkf <fkf.6106@gmail.com>
2024-10-17 19:54:06 +0200

    added flakes

---
+++ flakes/flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+            # stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              clang-tools
+              cmake
+              codespell
+              conan
+              cppcheck
+              doxygen
+              gtest
+              lcov
+              vcpkg
+              vcpkg-tool
+              gcc
+              stdenv.cc
+              stdenv.cc.libc stdenv.cc.libc_dev            
+              onefetch
+              glib 
+              glibc
+              tree
+      
+      ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+          };
+      });
+    };
+}
+
