53ec98163e1f815bdca21fc15a7e933a6cd6c3c5
fruity-fkf <fruity.fkf@proton.me>
2024-10-24 15:16:48 +0200

    zsh

--- homeManager/.config/home-manager/home.nix
+++ homeManager/.config/home-manager/home.nix
@@ -108,9 +108,24 @@
         success_symbol = "[➜](bold green)";
         error_symbol = "[➜](bold red)";
       };
-
       # package.disabled = true;
     };
+  };
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+    history = {
+      size = 10000;
+      path = "~/.config/zsh/history";
+    };
+
   };
 
 }
