07b8e81f9d7fe14b62f02b457f1ab6f5f414eb45
fruity-fkf <fruity.fkf@proton.me>
2024-10-27 19:48:49 +0100

    WHERE DO I FIND A CUTE BF

--- homeManager/.config/home-manager/shell-stuff.nix
+++ homeManager/.config/home-manager/shell-stuff.nix
@@ -1,19 +1,19 @@
 { pkgs, lib, ... }:
 {
-  programs.starship = {
-    enable = true;
-    # Configuration written to ~/.config/starship.toml
-    settings = {
-      add_newline = true;
-
-      character = {
-        success_symbol = "[➜](bold green)";
-        error_symbol = "[➜](bold red)";
-      };
-      # package.disabled = true;
-    };
-  };
-
+  # programs.starship = {
+  #   enable = true;
+  #   # Configuration written to ~/.config/starship.toml
+  #   settings = {
+  #     add_newline = true;
+  #
+  #     character = {
+  #       success_symbol = "[➜](bold green)";
+  #       error_symbol = "[➜](bold red)";
+  #     };
+  #     # package.disabled = true;
+  #   };
+  # };
+  #
   # programs.starship = {
   #   enable = true;
   #   enableFishIntegration = true;
