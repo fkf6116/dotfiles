f81e93e3a59feb7d27ef01ce6c08b5a34472e295
fruity-fkf <fruity.fkf@proton.me>
2024-11-06 19:54:33 +0100

    hypr

--- configuration.nix
+++ configuration.nix
@@ -306,12 +306,12 @@ in
   #
   # };
   #
-  # programs.steam = {
-  #   enable = true;
-  #   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #   dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  #   localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  # };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   fonts = {
     packages = with pkgs; [
