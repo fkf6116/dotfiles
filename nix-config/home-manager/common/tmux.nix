d133e7b9e0df2f212fd1b1b6d0599597a2de4f68
fruity-fkf <fruity.fkf@proton.me>
2025-08-08 05:10:11 +0200

    fixed tmux

--- nix-config/home-manager/common/tmux.nix
+++ nix-config/home-manager/common/tmux.nix
@@ -8,21 +8,11 @@ programs.tmux = {
     baseIndex = 1;
     prefix = "C-Space";
     keyMode = "vi";
-    terminal = "screen-256color"; # Also applies to xterm-256color due to terminal-features override
-
-
-
 extraConfig = ''
-  set-option -sa terminal-features ',xterm-256color:RGB'
-  set-option -a terminal-features 'screen-256color:RGB'
-
-  set-option -g allow-passthrough on
-  bind-key C-Space send-prefix
+set -g default-terminal "tmux-256color"
+set -ga terminal-overrides ",xterm-256color:Tc"
 
   set -g renumber-windows on
-  set-window-option -g mode-keys vi
-
-  # Status bar styles
   set -g status-position top
   set -g status-justify absolute-centre
   set -g status-style "fg=color7 bg=default"
@@ -31,22 +21,15 @@ extraConfig = ''
   set -g status-left-style "fg=color8"
   set -g status-right-length 0
   set -g status-left-length 100
-
-  setw -g window-status-current-style "fg=colour6 bg=default bold"
   setw -g window-status-current-format "#I:#W "
   setw -g window-status-style "fg=color8"
-
-  # Vim-like pane switching
   bind -r ^ last-window
   bind -r k select-pane -U
   bind -r j select-pane -D
   bind -r h select-pane -L
   bind -r l select-pane -R
-
-  # Pane splitting in current path
   bind v split-window -v -c "#{pane_current_path}"
   bind h split-window -h -c "#{pane_current_path}"
-
   # Toggle status bar
   bind-key b set-option status
 '';
